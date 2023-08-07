70013e3311b8fcdc0a40a450f9bf5457b591c8e2
Angel <angelmendez00512@gmail.com>
2023-08-07 11:25:54 -0600

    corrigiendo prpiedades

--- models/gms_propiedades.py
+++ models/gms_propiedades.py
@@ -7,5 +7,5 @@ class Propiedades(models.Model):
     name = fields.Char(string='Nombre')
     formula = fields.Text(string='Fórmula')
     producto_id = fields.Many2one('product.template', string='Producto')
-    unidad_medida = fields.Many2one('uom.uom', string='Unidad de medida') # asumiendo que deseas una unidad de medida estándar
+    umbral_tolerancia = fields.Float(string='Umbral de tolerancia')
     propiedad = fields.Char(string='Propiedad')
