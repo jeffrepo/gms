400ea009a7c006eb2560e3048ff55a1b9980ad1c
Angel <angelmendez00512@gmail.com>
2023-08-09 08:10:14 -0600

    Corrigiendo camiones, agregando satusbar a viajes

--- models/gms_viajes.py
+++ models/gms_viajes.py
@@ -4,10 +4,28 @@ class Viajes(models.Model):
     _name = 'gms.viaje'
     _description = 'Viaje'
 
-    # Agrega los campos que necesites para el modelo 'gms.viajes'
-
     name = fields.Char(string='Nombre del viaje')
     duracion_viaje = fields.Float(string='Duración del Viaje')
     gastos_combustible = fields.Float(string='Gastos de Combustible')
     gastos_peaje = fields.Float(string='Gastos de Peaje')
     observaciones = fields.Text(string='Observaciones')
+
+    state = fields.Selection([
+        ('borrador', 'Borrador'),
+        ('proceso', 'Proceso'),
+        ('liquidado', 'Liquidado'),
+        ('cancelado', 'Cancelado'),
+        ('terminado', 'Terminado')
+    ], string='Estado', default='borrador', required=True)
+
+    def action_confirm(self):
+        self.write({'state': 'proceso'})
+
+    def action_paid(self):
+        self.write({'state': 'liquidado'})
+
+    def action_cancel(self):
+        self.write({'state': 'cancelado'})
+
+    def action_done(self):
+        self.write({'state': 'terminado'})
