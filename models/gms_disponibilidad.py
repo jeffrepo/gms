101821f9dffb8d4ebb75f8cff092aff47b4f4a29
Angel <angelmendez00512@gmail.com>
2023-08-23 12:46:17 -0600

    corrigiendo  vistas y atributos

--- models/gms_disponibilidad.py
+++ models/gms_disponibilidad.py
@@ -3,7 +3,9 @@ from odoo import models, fields
 class CamionesDisponibilidad(models.Model):
     _name = 'gms.camiones.disponibilidad'
     _description = 'Disponibilidad de Camiones'
-
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    
+    follower_ids = fields.Many2many('res.users', string='Followers')
     camion_id = fields.Many2one('gms.camiones', string='Camión', required=True)
     fecha_inicio = fields.Date(string='Fecha de Inicio', required=True)
 
--- models/gms_historial.py
+++ models/gms_historial.py
@@ -1,12 +1,15 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
-class HistorialCamiones(models.Model):
+class HistorialAgendasCamiones(models.Model):
     _name = 'gms.historial'
-    _description = 'Historial de Camiones'
+    _description = 'Historial de Agendas de Camiones'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
+    follower_ids = fields.Many2many('res.users', string='Followers')
     camion_id = fields.Many2one('gms.camiones', string='Camión', required=True)
-    fecha_evento = fields.Date(string='Fecha del Evento', required=True)
-    evento = fields.Text(string='Evento', required=True)
+    fecha_hora_liberacion = fields.Datetime(string='Fecha y Hora de Liberación', required=True)
+    fecha = fields.Date(string='Fecha')
+    agenda_id = fields.Many2one('gms.agenda', string='Agenda', required=True)  # Cambiamos el modelo relacionado
 
     # Restricción para asegurarse de que el campo disponibilidad_id esté relacionado con el camión correcto
     _sql_constraints = [
