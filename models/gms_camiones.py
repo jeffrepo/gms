5e733a7da709b94540d3236c3e1355cf68dca450
Angel <angelmendez00512@gmail.com>
2023-08-28 16:12:39 -0600

    gms.agenda terminado

--- models/gms_camiones.py
+++ models/gms_camiones.py
@@ -4,30 +4,40 @@ class Camiones(models.Model):
     _name = 'gms.camiones'
     _description = 'Camiones'
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    _rec_name ="matricula"
+    _rec_name = "matricula"
 
     follower_ids = fields.Many2many('res.users', string='Followers')
-    nombre = fields.Char(string='Nombre', tracking ="1")
-    matricula = fields.Char(string='Matrícula', tracking ="1")
-    capacidad_kgs = fields.Float(string='Capacidad en Kgs', tracking ="1")
-    minimo_carga_kgs = fields.Float(string='Mínimo de Carga en Kgs', tracking ="1")
-    conductor_id = fields.Many2one('res.partner', string='Conductor', tracking ="1")
-    estado = fields.Selection([
-        ('disponible', 'Disponible'),
-        ('no_disponible', 'No Disponible')
-    ], string='Estado', default='disponible')
-    transportista_id = fields.Many2one('res.partner', string='Transportista', readonly=True, tracking ="1")
+    nombre = fields.Char(string='Nombre', tracking="1")
+    matricula = fields.Char(string='Matrícula', tracking="1")
+    capacidad_kgs = fields.Float(string='Capacidad en Kgs', tracking="1")
+    minimo_carga_kgs = fields.Float(string='Mínimo de Carga en Kgs', tracking="1")
+    transportista_id = fields.Many2one('res.partner', string='Transportista', tracking="1")
+    conductor_id = fields.Many2one('res.partner', string='Conductor', readonly=True, tracking="1")
+    disponible = fields.Boolean(string='Disponible', default=True, tracking="1")
+    ocupado = fields.Boolean(string='Ocupado', default=False, tracking="1")
+
     
+    def action_hacer_disponible(self):
+        for camion in self:
+            if not camion.disponible:
+                camion.write({'disponible': True})
+                fecha_hora_actual = fields.Datetime.now()
+                disponibilidad_vals = {
+                    'camion_id': camion.id,
+                    'estado': 'disponible',
+                    'fecha_hora_liberacion': fecha_hora_actual,
+                    # Agrega otros campos de disponibilidad aquí según sea necesario
+                }
+                self.env['gms.camiones.disponibilidad'].create(disponibilidad_vals)
 
-    @api.model
-    def create(self, vals):
-        # Crea el camión
-        camion = super(Camiones, self).create(vals)
-
-        # Crea un registro en 'gms.camiones.disponibilidad' usando el id del camión
-        disponibilidad_camion = self.env['gms.camiones.disponibilidad'].create({
-            'camion_id': camion.id,
-            'fecha_inicio': fields.Date.today(),
-        })
-
-        return camion
+    
+    def action_hacer_ocupado(self):
+        for camion in self:
+            if camion.disponible:
+                camion.write({'disponible': False})
+                disponibilidad = camion.env['gms.camiones.disponibilidad'].search([
+                    ('camion_id', '=', camion.id),
+                    ('estado', '=', 'disponible')
+                ], limit=1)
+                if disponibilidad:
+                    disponibilidad.write({'estado': 'ocupado'})
--- models/gms_disponibilidad.py
+++ models/gms_disponibilidad.py
@@ -5,11 +5,12 @@ class CamionesDisponibilidad(models.Model):
     _description = 'Disponibilidad de Camiones'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name ="camion_id"
+    _order = 'fecha_hora_liberacion DESC' 
     
     follower_ids = fields.Many2many('res.users', string='Followers')
-    camion_id = fields.Many2one('gms.camiones', string='Camión', required=True)
-    fecha_inicio = fields.Date(string='Fecha de Inicio')
-
+    camion_id = fields.Many2one('gms.camiones', string='Camión', required=True, order="fecha_hora_liberacion desc")
+    fecha_hora_liberacion = fields.Datetime(string='Fecha y Hora de Liberación')
+    transportista_id = fields.Many2one('res.partner', string='Trasportista', tracking="1")
     estado = fields.Selection([
         ('ocupado', 'Ocupado'),
         ('disponible', 'Disponible')
