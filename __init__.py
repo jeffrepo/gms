e30ff788af425db403a7cbc45a08fb96fbafaf46
Angel Méndez <79428530+angelmendez43@users.noreply.github.com>
2023-10-23 11:58:03 -0600

    Add files via upload
     AGENDA / VIAJES / PICKINGS P2  y purcharse

--- __init__.py
+++ __init__.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
-from . import models
-
+from . import gms_camiones
+from . import gms_propiedades
+from . import gms_rutas
+from . import gms_viajes
+from . import product_template
+from . import res_partner
+from . import gms
+from . import gms_gastoViaje
+from . import gms_agenda
+from . import gms_disponibilidad
+from . import gms_disponibilidad_2
+from . import gms_historial
+from . import gms_stock_picking
+from . import gms_sale_order
+from . import purchase_order
+from . import gms_medidas_propiedad
