3e9e7234348dc44dfddf676c37b23c0b6d4033e8
angel <angelmendez00512@gmail.com>
2023-11-02 23:26:06 -0600

    DESARROLLOS GMS AGENDA / VIAJES / PICKINGS P3

--- gms_tolva.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class Tolva(models.Model):
-    _name = 'gms.tolva'
-    _description = 'Modelo para las tolvas'
-
-    name = fields.Char(string='Tolva')
-    descripcion = fields.Text(string='Descripción')
